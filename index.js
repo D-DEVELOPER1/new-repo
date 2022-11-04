008b5bc5f598961c3d90939485a8bdefc03c3f24
ogunleye <tunde200.james@gmail.com>
2022-11-04 15:47:57 +0100

    my new commit

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const routes = require("./routes/flightRoute");
 
 const app = express();
 
+flights = [{
+    title: "flight to canada",
+    time: "1pm",
+    price: 26000,
+    date: "26-06-2022"
+}]
+
 app.use(json());
 
 app.use("/", routes);
 
+app.get('/', (req, res) => {
+    res.get(flights)
+
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
