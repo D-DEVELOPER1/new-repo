bb35677ed84012842bbef2f8faa4765a63c7d7b0
ogunleye <tunde200.james@gmail.com>
2022-11-05 01:16:27 +0100

    my commit

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ const routes = require("./routes/flightRoute");
 
 const app = express();
 
-flights = [{
+const value = [{
     title: "flight to canada",
     time: "1pm",
     price: 26000,
     date: "26-06-2022"
 }]
-
-app.use(json());
-
-app.use("/", routes);
-
-app.get('/', (req, res) => {
-    res.get(flights)
-
+app.get("/all", (req, res) => {
+    res.send(value)
 })
+app.get("/fight/:date", (req, res) => {
+    const course = value.find(c = () => c.date === parseInt(req.params.date))
+
+    if (!course) {
+        res.status(404).send("the course with the title is not found")
+    } else {
+        res.send(course)
+    }
+})
+app.post("/fight/post", (req, res) => {
+    const newCourse = [{
+        title: req.body.title,
+        time: req.body.time,
+        price: req.body.price,
+        date: req.body.date
+    }]
+    course.push(newCourse)
+    req.send(course)
+})
+app.use(json());
 
 const port = process.env.PORT || 3000;
 
